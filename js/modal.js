a6076c5ce41aed148938a1e126d13e876e078d1a
Andrii Tsokur <tsokur@gmail.com>
2023-01-16 16:15:38 +0200

    Adds forms and modal window

---
+++ js/modal.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Modal
+(() => {
+	const refs = {
+		openModalBtns: document.querySelectorAll('[data-modal-open]'),
+		closeModalBtn: document.querySelector('[data-modal-close]'),
+		modal: document.querySelector('[data-modal]'),
+	};
+
+	console.log(refs.openModalBtns.length);
+
+	for (let i = 0; i < refs.openModalBtns.length; i++) {
+		refs.openModalBtns[i].addEventListener('click', toggleModal);
+	}
+
+	refs.closeModalBtn.addEventListener('click', toggleModal);
+
+	function toggleModal() {
+		refs.modal.classList.toggle('is-hidden');
+		// document.querySelector('html').classList.toggle('is-frozen');
+	}
+})();
